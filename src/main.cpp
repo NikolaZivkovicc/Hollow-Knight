3de406493dbfed9a35b169604bc9438aa4f835ed
NikolaZivkovicc <zivkovicn558@gmail.com>
2024-04-11 21:28:48 +0200

    Promenio ime project base-a i prozora

--- src/main.cpp
+++ src/main.cpp
@@ -153,7 +153,7 @@ int main() {
 
     // glfw window creation
     // --------------------
-    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Hollow knight", NULL, NULL);
     if (window == NULL) {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
